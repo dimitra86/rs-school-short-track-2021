c46af40d95b5bacabaa52fb9f08557fd880e8786
Dzmitry Nikalayeu <dimitra86@list.ru>
2021-04-07 22:47:30 +0300

    feat: finish 05

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,20 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const str = `${n}`;
+  let nummin = str[0];
+  for (let i = 0; i < str.length; i++) {
+    if (nummin > str[i]) {
+      nummin = str[i];
+    }
+  }
+
+  const arr = str.split('');
+  const index = arr.indexOf(nummin);
+  delete arr[index];
+
+  return arr.join('') / 1;
 }
 
 module.exports = deleteDigit;
