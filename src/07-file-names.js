34cb3d06ab03548a2e46db3ad444d8f1754a83b4
Dzmitry Nikalayeu <dimitra86@list.ru>
2021-04-11 16:28:58 +0300

    feat: finish 07

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,28 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const arr2 = [];
+  let k = -1;
+  const doc = 'doc';
+  const c = names.map((item) => {
+    if (item === doc) {
+      k++;
+      return k;
+    }
+    return null;
+  });
+
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] === 'doc(1)') {
+      arr2.push('doc(1)(1)');
+    } else if (c[i] === 0 || c[i] === undefined || c[i] === null) {
+      arr2.push(names[i]);
+    } else {
+      arr2.push(`${names[i]}(${c[i]})`);
+    }
+  }
+  return arr2;
 }
 
 module.exports = renameFiles;
