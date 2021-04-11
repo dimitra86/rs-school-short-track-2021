d48a5ab9d26176effb9ed24122f7470f7cecee8e
Dzmitry Nikalayeu <dimitra86@list.ru>
2021-04-11 18:01:14 +0300

    feat: finish 09

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,25 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  const str = n.toString();
+  let str1 = '';
+  let str2 = '';
+  let str3 = '';
+  if (str.length === 3) {
+    str1 = str[0] / 1 + str[1] / 1 + str[2] / 1;
+    if (str1 < 10) { return str1; }
+    str2 = str1.toString();
+    str3 = str2[0] / 1 + str2[1] / 1;
+    if (str3 < 10) { return str3; }
+  } else if (str.length === 2) {
+    str1 = str[0] / 1 + str[1] / 1;
+    if (str1 < 10) { return str1; }
+    str2 = str1.toString();
+    str3 = str2[0] / 1 + str2[1] / 1;
+    if (str3 < 10) { return str3; }
+  }
+  return null;
 }
 
 module.exports = getSumOfDigits;
